372bcc28efb85d546badb23da2a7eab0e98d9198
developerfatma <bulutfatmatugce@yahoo.com>
2025-05-16 17:20:52 +0300

    roll-a-ball projesi yuklendi.

---
+++ Assets/Scripts/KupKontrol.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KupKontrol : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(15 * 0.01f, 30 * 0.01f, 45 * 0.01f);
+    }
+}
---
+++ Assets/Scripts/TopKontrol.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TopKontrol : MonoBehaviour
+{
+    public Rigidbody fizik;
+    public int hiz;
+    public int sayac;
+    public int objeSayisi;
+    public Text PuanText;
+    public Text OyunBittiText;
+
+
+    void Start()
+    {
+        fizik=GetComponent<Rigidbody>();      
+    }
+
+    
+    void FixedUpdate()
+    {
+        float yatay = Input.GetAxis("Horizontal");
+        float dikey = Input.GetAxis("Vertical");
+        Vector3 vektor = new Vector3(yatay, 0, dikey);
+        fizik.AddForce(vektor*hiz); 
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+       other.gameObject.SetActive(false);
+       sayac ++;
+        PuanText.text = "Puan : " + sayac;
+        if(sayac == objeSayisi)
+        {
+            OyunBittiText.gameObject.SetActive(true);
+        }
+       
+    }
+
+}
